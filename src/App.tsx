03fb6fe14d282f64d3c779f6c24e072d278a6088
Trevor Brunette <trevorbrunette@gmail.com>
2021-09-24 17:26:36 -0400

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Search from './Search/search'
 import './App.css';
 
 function App() {
@@ -7,12 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the template for a node-react-ts project. You can edit <code>src/App.tsx</code> and save to reload the page.
-          <br/>
-          See the installation guide in the repo for information on how to set up the project for development on your system.
-          Help with staging
-        </p>
+        <Search />
         <a
           className="App-link"
           href="https://github.com/rpi-tools/RTools"
