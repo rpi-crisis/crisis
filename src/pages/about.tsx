ddf616a972937d6678503f8d359dd4cbebe82de1
cxider <theqharprlos@gmail.com>
2021-10-04 15:25:13 -0400

    Added Nav-bar

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,35 @@
 import React, { FC } from "react";
 import {Link} from "react-router-dom";
-import '../App.css';
 import './about.css';
+import NavBar from "../navbar/navbar";
 
 const AboutPage: FC = () => {
+  let pages: {text:string, path:string, active:boolean, external?:string}[] = [
+    {
+      text:"Home",
+      path:"/",
+      active:false,
+    },
+    {
+      text:"About",
+      path:"/about",
+      active:true,
+    },
+    {
+      text:"GitHub",
+      path:"/",
+      active:false,
+      external:"https://github.com/rpi-crisis/crisis"
+    },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar pages={pages} />
         <h3>About page</h3>
         <p className="about-text">CRISIS is an open source RCOS project: a website that houses various course and major related tools
           for my college, Rensselaer Polytechnic Institute, since we believe that the offered tools are insufficient.</p>
-        <Link
-          className="App-link"
-          to="/" >
-          Home
-        </Link>
       </header>
     </div>
   );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,39 @@
 import React, { FC } from "react";
-import {Link} from "react-router-dom";
 import logo from '../res/images/logo.svg';
-import '../App.css';
 import './home.css';
 
+import NavBar from '../navbar/navbar';
+
 const HomePage: FC = () => {
+
+  let pages: {text:string, path:string, active:boolean, external?:string}[] = [
+    {
+      text:"Home",
+      path:"/",
+      active:true,
+    },
+    {
+      text:"About",
+      path:"/about",
+      active:false,
+    },
+    {
+      text:"GitHub",
+      path:"/",
+      active:false,
+      external:"https://github.com/rpi-crisis/crisis"
+    },
+  ]
+
   return (
       <div className="App">
         <header className="App-header">
+          <NavBar pages={pages}/>
           <img src={logo} className="App-logo" alt="logo" />
           <h3>CRISIS - Correcting RPI's Insufferable SIS</h3>
           <p className="home-text">
             This site is still in development. Check out or contribute to our GitHub to see the progress!
           </p>
-          <Link
-            className="App-link"
-            to="/about" >
-            About
-          </Link>
-          <a
-              className="App-link"
-              href="https://github.com/rpi-crisis/crisis"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            GitHub Repo
-          </a>
         </header>
       </div>
   )
