5de79c9c18ad77b8af770de6082368e1a7be1cb2
cxider <theqharprlos@gmail.com>
2021-10-25 22:44:13 -0400

    Modified Card to Take Content
    also it now has a hover animation

--- src/card/card.tsx
+++ src/card/card.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from "react";
+import React, { FC, useRef } from "react";
 import './card.css';
 
-const Card: FC<{head:string}> = ({head}) => {
+const Card: FC<{head:string, content:string}> = ({head,content}) => {
   return (
     <div className='card'>
-      <div>
+      <div className='head'>
         {head}
+      </div>
+      <div className='content'>
+        {content}
       </div>
     </div>
   )
