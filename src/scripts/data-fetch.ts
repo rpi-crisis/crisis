264cc2433b8c169bde70ebd4416a2d76d89b6a50
Trevor Brunette <trevorbrunette@gmail.com>
2021-10-27 02:03:52 -0400

    Small refactor. Preparing for testing out better solutions to storing data locally.

--- src/scripts/data-fetch.ts
+++ src/scripts/data-fetch.ts
@@ -9,8 +9,6 @@ const version_name:string = "meta";
 let db_version:string = "";
 let local_version:string = "";
 
-let data_json = []
-
 let is_init:boolean = false;
 
 function init(){
@@ -24,15 +22,15 @@ function init(){
 function fetchVersion() {
   fetch(filepath+version_name)
     .then(version => { version.text()
-        .then((result_text) => {
-        db_version = result_text;
+        .then((version_text) => {
+        db_version = version_text;
+        // Debug logging
         console.log("Found local_version: " + local_version);
         console.log("Found db_version: " + db_version);
         if (local_version !== db_version) {
           fetchData();
           console.log("Updating local_version");
         } else {
-          data_json = JSON.parse(localStorage.getItem("courses_json") as string);
           console.log("No update needed");
         }
       });
@@ -47,11 +45,11 @@ function fetchData() {
   fetch(filepath + courses_name)
     .then(response => response.json())
     .then(data => {
-      data_json = data;
       localStorage.setItem("courses_json", JSON.stringify(data));
       localStorage.setItem("courses_version", db_version);
+      // Debug logging
       console.log("courses.json fetched");
-      console.log(data_json);
+      console.log(data);
     })
     .catch(error => {
       console.error(error)
