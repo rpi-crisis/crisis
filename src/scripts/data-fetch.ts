9b7ac627ab4c55aaf4ef5caf56f05fddb827e225
Trevor Brunette <trevorbrunette@gmail.com>
2021-10-27 03:10:26 -0400

    Added some comments, more debugging prints, and a "ready" boolean, which has the capability of being calling an external update hook. Fixed some formatting and syntax.

--- src/scripts/data-fetch.ts
+++ src/scripts/data-fetch.ts
@@ -11,37 +11,41 @@ const version_name:string = "meta";
 let db_version:string = "";
 let local_version:string = "";
 
+// Will be set to true while the function is running. Helps ensure that the function is not called multiple times.
 let is_init:boolean = false;
+// Will be set to true when there are the necessary stringified json files in the local storage.
+let ready:boolean = false;
+
+// This function may be overwritten if we need to use an update hook.
+function updateReady(value:boolean){
+  ready = value;
+}
 
 function init(){
-  if(!is_init) {
+  if(!is_init && !ready) {
     is_init = true;
     localforage.getItem("data_version")
       .then((data_version_value)=>{
         local_version = data_version_value as string;
         fetchVersion();
-    })
-    //local_version = localStorage.getItem("data_version") as string;
-    //fetchVersion();
+    });
   }
 }
 
 function fetchVersion() {
   fetch(filepath+version_name)
-    .then(version => { version.text()
-        .then((version_text) => {
+    .then(response => { response.text()
+      .then((version_text) => {
         db_version = version_text;
-        // Debug logging
-        console.log("Found local_version: " + local_version);
-        console.log("Found db_version: " + db_version);
+        console.log("Found local_version: " + local_version); // Debug logging
+        console.log("Found db_version: " + db_version); // Debug logging
         if (local_version !== db_version) {
           fetchData();
-          console.log("Updating local_version");
         } else {
-          console.log("No update needed");
+          updateReady(true);
+          console.log("No update needed"); // Debug logging
         }
       });
-
     }).catch((error) => {
     console.log("Failed to get version: " + error);
     is_init = false;
@@ -52,14 +56,18 @@ function fetchData() {
   fetch(filepath + courses_name)
     .then(response => response.json())
     .then(data => {
-      localforage.setItem("courses_json", JSON.stringify(data));
+      local_version = db_version
       localforage.setItem("data_version", db_version);
-      // Debug logging
-      console.log("courses.json fetched");
-      console.log(data);
+      localforage.setItem("courses_json", JSON.stringify(data)).then(()=>{
+        updateReady(true)
+        console.log("Updated files to match server"); // Debug logging
+        console.log("Set local_version to: " + local_version); // Debug logging
+        console.log("File: courses.json fetched"); // Debug logging
+        // console.log(data); // Debug logging
+      });
     })
     .catch(error => {
-      console.error(error)
+      console.error("Failed to get data file: " + error);
       is_init = false;
     });
 }
