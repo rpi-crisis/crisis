0730e38eb8bfed4feaed523f297990a374ad838e
cxider <44309126+cxider@users.noreply.github.com>
2021-12-10 01:33:47 -0500

    Made Demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import HomePage from './pages/home/home';
 import AboutPage from './pages/about/about';
-import TestSearch from "./pages/test-search";
+//import TestSearch from "./pages/test-search";
 import init from "./scripts/data-fetch"
 
 function App() {
@@ -13,11 +13,10 @@ function App() {
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/search" component={TestSearch} />
         <Route render={() => <h1>Error 404: Page not found</h1>} />
       </Switch>
     </Router>
   );
 }
-
+//<Route path="/search" component={TestSearch} />
 export default App;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,55 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import logo from '../../res/images/kool-aid_cat.png';
 import './home.css';
 
+import { getJsonData } from "../../scripts/data-fetch";
+import { search } from "../../scripts/fuzzy-search";
+
 import NavBar from '../../components/navbar/navbar';
 import SearchBar from '../../components/search/search';
-
 import Pages from '../pageList'
+import Class from "../../components/class/class";
+
+interface InputState {
+  results: any[];
+  time: number;
+}
+
+let courses_json: any[] = []
+//let searched: boolean = false //TODO make it say loading before search completed
+
+getJsonData()
+  .then(res => {
+    courses_json = res;
+  }
+);
 
 const HomePage: FC = () => {
 
+  const [state, setState] = useState<InputState>({results:[], time:0});
 
-  let [searchbar_content, content_update] = useState("");
-  let [search_tags, update_tags] = useState<string[]>([]);
+  let [searchbar_content, content_update] = useState<string>("");
 
-  const search_function:()=>void = () => {
-    alert(searchbar_content);
+  const searchQuery = (query: string):void => {
+    if(query==="") {
+      setState({results: [], time: 0});
+      return;
+    }
+    let before = performance.now();
+    let results = search(query, courses_json, ["id", "title", "description"]).slice(0,5);
+    let after = performance.now();
+    console.log(results)
+    setState({results: results, time: Math.round(after-before)});
   }
+
+ const call_search = React.useCallback((value:string):void => {
+  searchQuery(value);
+}, []);
+
+  useEffect(() => {  
+    const timeOutId = setTimeout(() => call_search(searchbar_content),200);
+    return () => clearTimeout(timeOutId);
+  }, [searchbar_content,call_search]);
 
   return (
       <div className="App">
@@ -23,7 +57,8 @@ const HomePage: FC = () => {
           <NavBar pages={Pages("Home")}/>
           <img src={logo} className="App-logo" alt="logo" />
           <h3>CRISIS - Correcting RPI's Insufferable SIS</h3>
-          <SearchBar content_update={content_update} searchFunc={search_function} tags={search_tags} tag_update={update_tags}/>
+          <SearchBar content_update={content_update}/>
+	  {state.results.map((el,pos) => {return <Class key={pos} data={el}/>})}
         </header>
       </div>
   )
