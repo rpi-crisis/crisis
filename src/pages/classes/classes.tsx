3c9c1afa4fe2f823d56bb2351dd4abcb0b128307
Hamood <mumualafifi@gmail.com>
2022-03-18 17:03:48 -0400

    working on specific class page
    Now reads path

--- src/pages/classes/classes.tsx
+++ src/pages/classes/classes.tsx
@@ -6,64 +6,35 @@ import { getJsonData } from "../../scripts/data-fetch";
 import { search } from "../../scripts/fuzzy-search";
 
 import NavBar from "../../components/navbar/navbar";
-import SearchBar from "../../components/search/search";
 import Pages from "../pageList";
-import Class from "../../components/class/class";
-
-interface InputState {
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  results: any[];
-  time: number;
-}
-
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-let courses_json: any[] = [];
-//let searched: boolean = false //TODO make it say loading before search completed
-
-getJsonData()
-  .then(res => {
-    courses_json = res;
-  }
-  );
 
 const ClassesPage: FC = () => {
 
-  const [state, setState] = useState<InputState>({results:[], time:0});
-
-  const [searchbar_content, content_update] = useState<string>("");
-
-  const searchQuery = (query: string): void => {
-    if(query === "") {
-      setState({results: [], time: 0});
-      return;
-    }
-    const before = performance.now();
-    const results = search(query, courses_json, ["department"]).slice(0,7);
-    const after = performance.now();
-    console.log(results);
-    setState({results: results, time: Math.round(after - before)});
-  };
-
-  const call_search = React.useCallback((value: string): void => {
-    searchQuery(value);
-  }, []);
-
-  useEffect(() => {  
-    const timeOutId = setTimeout(() => call_search(searchbar_content),200);
-    return () => clearTimeout(timeOutId);
-  },[searchbar_content,call_search]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar pages={Pages("Classes")}/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h3>Enter the department you want</h3>
-        <SearchBar content_update={content_update}/>
-        {state.results.map((el,pos) => {return <Class key={pos} data={el}/>;})}
-      </header>
-    </div>
-  );
+  const pathname = window.location.pathname;
+  const classCode = pathname.substring(7);
+  
+  if(classCode.length != 8){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar pages={Pages("Classes")}/>
+          <h3>ERROR 404: PAGE NOT FOUND</h3>
+          invalid class code
+        </header>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar pages={Pages("Classes")}/>
+          <h3>{classCode}</h3>
+          WIP
+        </header>
+      </div>
+    );
+  }
 };
 
 export default ClassesPage;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -57,7 +57,6 @@ const HomePage: FC = () => {
     <div className="App">
       <header className="App-header">
         <NavBar pages={Pages("Home")}/>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h3>CRISIS - Correcting Rensselaer{"'"}s Insufferable SIS</h3>
         <SearchBar content_update={content_update}/>
         {state.results.map((el,pos) => {return <Class key={pos} data={el}/>;})}
