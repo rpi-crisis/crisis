aeb82f5f26d8fc316b2f4dff02424b9198eae93a
Hamood <mumualafifi@gmail.com>
2022-03-21 23:01:43 -0400

    updated to the main
    currently having plenty of problems including,
    "[state having any type]"
    and several problems in index.tsx

--- src/pages/classes/classes.tsx
+++ src/pages/classes/classes.tsx
@@ -6,50 +6,33 @@ import "./classes.css";
 import NavBar from "../../components/navbar/navbar";
 // import SearchBar from "../../components/search/search";
 import Pages from "../pageList";
-// import Class from "../../components/class/class";
-// import {Course} from "../../types";
-
-// interface InputState {
-//   results: Course[];
-// }
-//
-// const searcher = new CourseSearcher();
 
 const ClassesPage: FC = () => {
 
-  // const [state, setState] = useState<InputState>({results:[]});
-  //
-  // const [searchbar_content, content_update] = useState<string>("");
-  //
-  // const searchQuery = (query: string): void => {
-  //   if(query === "") {
-  //     setState({results: []});
-  //     return;
-  //   }
-  //   const results = searcher.search(query).slice(0,7);
-  //   console.log(results);
-  //   setState({results: results});
-  // };
-  //
-  // const call_search = React.useCallback((value: string): void => {
-  //   searchQuery(value);
-  // }, []);
-  //
-  // useEffect(() => {
-  //   const timeOutId = setTimeout(() => call_search(searchbar_content),200);
-  //   return () => clearTimeout(timeOutId);
-  // },[searchbar_content,call_search]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar pages={Pages("Classes")}/>
-        <h3>Enter the department you want</h3>
-        {/*<SearchBar content_update={content_update}/>*/}
-        {/*{state.results.map((el,pos) => {return <Class key={pos} data={el}/>;})}*/}
-      </header>
-    </div>
-  );
-};
-
+  const pathname = window.location.pathname;
+  const classCode = pathname.substring(7);
+  
+  if(classCode.length != 8){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar pages={Pages("Classes")}/>
+          <h3>ERROR 404: PAGE NOT FOUND</h3>
+          invalid class code
+        </header>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar pages={Pages("Classes")}/>
+          <h3>{classCode}</h3>
+          WIP
+        </header>
+      </div>
+    );
+  }
+}
 export default ClassesPage;
